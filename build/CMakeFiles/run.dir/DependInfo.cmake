0370279fdfe2fecf2fbe02da62436d12ce2bf4f2
root <root@localhost.localdomain>
2019-10-30 23:33:09 +0500

    initial commit

---
+++ build/CMakeFiles/run.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/Projects/Snake/src/Controller.cpp" "/root/Projects/Snake/build/CMakeFiles/run.dir/src/Controller.o"
+  "/root/Projects/Snake/src/Game.cpp" "/root/Projects/Snake/build/CMakeFiles/run.dir/src/Game.o"
+  "/root/Projects/Snake/src/Snake.cpp" "/root/Projects/Snake/build/CMakeFiles/run.dir/src/Snake.o"
+  "/root/Projects/Snake/src/Window.cpp" "/root/Projects/Snake/build/CMakeFiles/run.dir/src/Window.o"
+  "/root/Projects/Snake/src/main.cpp" "/root/Projects/Snake/build/CMakeFiles/run.dir/src/main.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././include"
+  ".././src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/run.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run.dir/src/main.o"
+  "CMakeFiles/run.dir/src/Game.o"
+  "CMakeFiles/run.dir/src/Window.o"
+  "CMakeFiles/run.dir/src/Controller.o"
+  "CMakeFiles/run.dir/src/Snake.o"
+  "run.pdb"
+  "run"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
